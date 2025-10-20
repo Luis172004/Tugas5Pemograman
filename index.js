7c21cfa6630e29a8ad1aabb50b7923722ae45a06
Luis172004 <mambrasar17@gmail.com>
2025-10-20 21:45:57 +0900

    Tugas
    Tugas Modul dan Package

---
+++ index.js
@@ -0,0 +1,9 @@
+const add = require("./add/add");
+const substract = require("./substract/substract")
+const multi = require("./multi/multi")
+const divide = require("./divide/divide")
+
+console.log(add(5, 7));
+console.log(substract(5, 7));
+console.log(multi(5, 7));
+console.log(divide(5, 7));
